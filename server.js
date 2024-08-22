0746274b7957956127487ceaf39d5549e64cda73
ramudu <nukathotiramudu@gmail.com>
2024-08-22 12:50:04 +0530

    sulution

---
+++ server.js
@@ -0,0 +1,51 @@
+const express=require('express')
+const {open}=require('sqlite')
+const sqlite3=require('sqlite3')
+const path=require('path')
+
+const app=express()
+
+const dbPath=path.join(__dirname,"ccdata.db")
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Adjust to match your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }));
+
+app.use(express.json())
+
+let db=null
+
+initiateAndStartDatabaseServer=async()=>{
+    try{
+        db=await open({
+            filename:dbPath,
+            driver:sqlite3.Database
+        })
+        app.listen(3000,()=>{
+            console.log('Backend Server is running at htpp://localhost:3000/')
+        })
+    } catch (e) {
+        console.log(`DB Error {e.message}`)
+        process.exit(1)
+    }
+}
+
+initiateAndStartDatabaseServer()
+
+
+app.get("/mentors",async(req,res)=>{
+   const mentirsQuery=`select * from mentors;`
+   const response=await db.all(mentirsQuery)
+   res.send(response)
+})
+
+
+app.get("/bookings",async(req,res)=>{
+    const mentirsQuery=`select * from Bookings;`
+    const response=await db.all(mentirsQuery)
+    res.send(response)
+ })
+
